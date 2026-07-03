a11edca7e603e08f0267687c33d9114b2ea99395
agung3956 <agung3956@gmail.com>
2026-07-03 22:27:44 +0700

    Support Vercel and GitHub Pages base paths

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/pengadaan/",
+  base: process.env.VERCEL ? "/" : "/pengadaan/",
   plugins: [react()],
 });
